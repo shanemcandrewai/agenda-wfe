5ec1c00fcf01746c195b71ffd8b6d346e4260f63
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2020-11-15 12:34:43 +0100

    simplify cgi

--- cgi-bin/testscr.py
+++ cgi-bin/testscr.py
@@ -1,7 +1,19 @@
+#! /usr/bin/env python
 import cgitb
 import cgi
 cgitb.enable()
 form = cgi.FieldStorage()
-print("Content-Type: text/html")
+print('Content-Type: text/html')
 print()
-print(form.getfirst("test-input"))
+print('<h1>CGI input test</h1>')
+print("<label for='test-id'>Enter some text:</label>")
+print("<input type='text' id='test-id' name='test-input' value='test-val' />")
+print("<script>")
+print("document.getElementById('test-id').addEventListener('keydown', event => {")
+print("console.log(event.key)}")
+#print("fetch(('cgi-bin/testscr.py'))")
+#print(".then(response => response.text())")
+#print(".then(data => document.body.innerHTML = data); });")
+print("</script>")
+#print(form.getfirst('test-inputx'))
+#print('test output')
